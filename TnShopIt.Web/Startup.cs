8ad97c3e1157d928a11224bd7b8a611f63adf269
ThanhNhan-PC\ThanhNhan <tthnhanit@gmail.com>
2016-09-16 05:39:40 -0700

    The First Commit

---
+++ TnShopIt.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TnShopIt.Web.Startup))]
+namespace TnShopIt.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
